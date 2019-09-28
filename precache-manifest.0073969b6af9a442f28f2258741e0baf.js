5c7a8a190c0904b122c1b4acc1dc1c02710bdc41
Andriy Kyrylenko <z9877969@ukr.net>
2019-09-28 22:43:24 +0300

    Updates

---
+++ precache-manifest.0073969b6af9a442f28f2258741e0baf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "900b68de3528781196d16386218fb7a6",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "e7ae62ab0501af22ce17",
+    "url": "/goit-react-hw-02-reader/static/css/main.9fbefc9d.chunk.css"
+  },
+  {
+    "revision": "30d5305f06f512a70f69",
+    "url": "/goit-react-hw-02-reader/static/js/2.5fa7cfb2.chunk.js"
+  },
+  {
+    "revision": "e7ae62ab0501af22ce17",
+    "url": "/goit-react-hw-02-reader/static/js/main.87555eb1.chunk.js"
+  },
+  {
+    "revision": "29b43a06855652c5d22f",
+    "url": "/goit-react-hw-02-reader/static/js/runtime-main.434e3f28.js"
+  }
+]);
